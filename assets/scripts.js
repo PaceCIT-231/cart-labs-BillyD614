a5bc272c66a4b47378ab11609132a365abf245ce
Billy <bd39023p@pace.edu>
2022-11-09 14:33:56 -0500

    added functions

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,9 +8,29 @@ function addToCart(cookie) {
         party press: 35
         chocolate chip: 25
     */
+
+
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
+    itemCount++
+    console.log(itemCount)
 
+    if (cookie == "peanut butter") {
+        currentPrice = 20 + currentPrice
+    }
+
+    if (cookie == "sandies") {
+        currentPrice = 30 + currentPrice
+    }
+
+    if (cookie == "party press") {
+        currentPrice = 35 + currentPrice
+    }
+
+    if (cookie == "chocolate chip") {
+        currentPrice = 25 + currentPrice
+    }
+    console.log(currentPrice)
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
 
@@ -18,6 +38,6 @@ function addToCart(cookie) {
 
 function checkout() {
     console.log('User is checking out.')
+    console.log('You are purchasing ' , itemCount, ' items! Your total costs is ', currentPrice , 'dollars!'  )
     //Let your customer know how many items they are purchasing and the price
-    
 }
